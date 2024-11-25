cd3adced2f17f4a2c792903a066d65af3de5b476
BrendaOrtega <bremin11.20.93@gmail.com>
2024-11-25 13:31:26 -0600

    analytics

--- app/root.tsx
+++ app/root.tsx
@@ -9,29 +9,16 @@ import {
 } from "@remix-run/react";
 
 import "./tailwind.css";
+import useGoogleTM from "./lib/useGoogleTM";
 
 export default function App() {
+  useGoogleTM();
   return (
     <>
       <html suppressHydrationWarning>
         <head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          {/* <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-BN504Z5FBK"
-          ></script>
-          <script>
-            {`
-      window.dataLayer = window.dataLayer || [];
-      function gtag() {
-        dataLayer.push(arguments);
-      }
-      gtag("js", new Date());
-
-      gtag("config", "G-BN504Z5FBK");
-      `}
-          </script> */}
           <Meta />
           <Links />
         </head>
