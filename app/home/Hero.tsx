7e5b4586ae989766696e262ee26d4696741205c5
BrendaOrtega <bremin11.20.93@gmail.com>
2024-12-16 13:09:33 -0600

    home fixes

--- app/home/Hero.tsx
+++ app/home/Hero.tsx
@@ -9,7 +9,7 @@ const words = `  Aprende animaciones web con React`;
 export const Hero = ({ children }: { children?: ReactNode }) => {
   return (
     <section className="bg-magic flex flex-wrap md:flex-nowrap  bg-contain bg-no-repeat bg-right pt-12 md:pt-[120px] min-h-[90vh] lg:min-h-[95vh]  ">
-      <div className="w-full px-6 pt-0 md:px-0 lg:max-w-7xl mx-auto flex flex-col items-center ">
+      <div className="w-full px-6 pt-0 md:px-0 lg:max-w-7xl mx-auto flex flex-col items-center justify-center ">
         <Tools />
         <div className="w-full lg:w-[70%] mx-auto -mt-8 md:mt-12 ">
           <TextGenerateEffect words={words} />
