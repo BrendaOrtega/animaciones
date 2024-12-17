a79bdc6cf071fee633e693c1717d298e57b4dac1
BrendaOrtega <bremin11.20.93@gmail.com>
2024-12-17 10:34:40 -0600

    404

--- app/root.tsx
+++ app/root.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import {
+  isRouteErrorResponse,
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useRouteError,
 } from "@remix-run/react";
 import "./tailwind.css";
 import useGoogleTM from "./lib/useGoogleTM";
@@ -40,4 +42,63 @@ export default function App() {
       </html>
     </>
   );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    return (
+      <html>
+        <head>
+          <meta charset="UTF-8" />
+        </head>
+        <body>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100vh",
+              textAlign: "center",
+            }}
+          >
+            <div>
+              <img style={{ width: "180px" }} src="/404.svg" />
+              <h1 style={{ fontFamily: "monospace" }}>
+                Ups ¡Esta página no existe!
+              </h1>
+              <a href="/">
+                <button
+                  style={{
+                    height: "48px",
+                    borderRadius: "24px",
+                    backgroundColor: "#5158F6",
+                    padding: "0 16px",
+                    color: "white",
+                    border: "none",
+                    marginTop: "32px",
+                    cursor: "pointer",
+                  }}
+                >
+                  Volver a la página principal
+                </button>
+              </a>
+            </div>
+          </div>
+        </body>
+      </html>
+    );
+  } else if (error instanceof Error) {
+    return (
+      <div>
+        <h1>como estan</h1>
+        <p>hola{error.message}</p>
+        <p>The stack trace is:</p>
+        <pre>{error.stack}</pre>
+      </div>
+    );
+  } else {
+    return <h1>Unknown Error</h1>;
+  }
 }
