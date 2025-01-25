7c1a7b614bb1f2890d88b1a4bd04d2e3fa7fb400
Blissito <fixtergeek@gmail.com>
2025-01-25 16:11:47 -0600

    working navbar

--- app/components/AnimatedNavBar.tsx
+++ app/components/AnimatedNavBar.tsx
@@ -1,6 +1,5 @@
-import { Form } from "@remix-run/react";
-import { ReactNode, useState } from "react";
 import { cn } from "~/lib/utils";
+import { ReactNode, useState } from "react";
 import {
   FaArrowRight,
   FaChevronDown,
@@ -127,10 +126,10 @@ const Menu = ({
 
 const SignInButtons = () => {
   return (
-    <Form className="flex gap-1">
+    <form className="flex gap-1">
       <Button mode="shadow">Log in</Button>
       <Button mode="primary">Sign up</Button>
-    </Form>
+    </form>
   );
 };
 
@@ -191,8 +190,8 @@ const Logo = () => {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M32.5 64C50.1731 64 64.5 49.6731 64.5 32C64.5 20.1555 58.0648 9.81393 48.5 4.28099V31.9999V47.9998H40.5V45.8594C38.1466 47.2207 35.4143 47.9999 32.5 47.9999C23.6634 47.9999 16.5 40.8364 16.5 31.9999C16.5 23.1633 23.6634 15.9999 32.5 15.9999C35.4143 15.9999 38.1466 16.779 40.5 18.1404V1.00812C37.943 0.350018 35.2624 0 32.5 0C14.8269 0 0.500038 14.3269 0.500038 32C0.500038 49.6731 14.8269 64 32.5 64Z"
         fill="black"
       />
