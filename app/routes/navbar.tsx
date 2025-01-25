1ae4dd3989e71821613429239fe48689958fc159
Blissito <fixtergeek@gmail.com>
2025-01-25 12:28:22 -0600

    animated navbar part 2 complete

---
+++ app/routes/navbar.tsx
@@ -0,0 +1,9 @@
+import { AnimatedNavBar } from "~/components/AnimatedNavBar";
+
+export default function Page() {
+  return (
+    <article className="h-screen bg-[#faefe5]">
+      <AnimatedNavBar />;
+    </article>
+  );
+}
